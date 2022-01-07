602d39ee98c7b7675f22b9512ee768c620be7134
Thangpro2010 <72727519+Thangpro2010@users.noreply.github.com>
2022-01-07 17:39:04 +0700

    font-end about, contact

--- ArtGallery.WebApp/Controllers/HomeController.cs
+++ ArtGallery.WebApp/Controllers/HomeController.cs
@@ -32,6 +32,16 @@ namespace ArtGallery.WebApp.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
