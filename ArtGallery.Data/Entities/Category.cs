8461ae0cf5381689125c14aa10818e534ca34682
vinhvizg <vinhvizg1040@gmail.com>
2022-01-10 16:25:59 +0700

    update service category

--- ArtGallery.Data/Entities/Category.cs
+++ ArtGallery.Data/Entities/Category.cs
@@ -10,6 +10,7 @@ namespace ArtGallery.Data.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public List<Product> Products { get; set; }
     }
 }
